8e43227f4a44614a0fcfc20d0f4d634e55f20314
Colton <76969314+becksteadc@users.noreply.github.com>
2026-01-25 14:34:29 -0800

    A little work. Fixed segmentation faults that were caused by the table callback not having the mouse calls implemented yet.

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 UITextbox *textbox;
 UIButton *button;
 UILabel *label;
-UICode *dir_listing;
-//UISplitPane *split;
+UITable *dir_listing;
 
 struct String_Linked_List {
 	char *str;
@@ -18,53 +18,48 @@ struct String_Linked_List {
 	struct String_Linked_List *next;
 };
 
+struct String_Linked_List Global_Dir_Listing;
 /* Event callback functions. (listeners) */
 int ButtonMessage(UIElement *element, UIMessage message, int di, void *dp);
 int TextboxMessage(UIElement *element, UIMessage message, int di, void *dp);
+int TableMessage(UIElement *element, UIMessage message, int di, void *dp);
+int TableContents(UIElement *element, UIMessage message, int di, void *dp);
 
 /* background functionality & helper functions. */
-int list_dir(const char *path, char **result);
+int list_dir(const char *path, struct String_Linked_List *result);
 int pack_SLL_to_CString(struct String_Linked_List *head, char **result);
 void cleanup_SLL(struct String_Linked_List *list);
+
+void cwb_init_globals(void)
+{
+	Global_Dir_Listing.str = "Default";
+	Global_Dir_Listing.length = sizeof("Default");
+	Global_Dir_Listing.next = NULL;
+}
 
 /* Entry Point -- for linux support, add conditional compilation here. */
 int WinMain(HINSTANCE instance, HINSTANCE previousInstance,
 			LPSTR commandline, int showCommand)
 {
 	UIInitialise();
+	cwb_init_globals();
 	UIWindow *window = UIWindowCreate(0, 0, "hello", 640, 480);
-
 	UISplitPane *split1 = UISplitPaneCreate(&window->e, UI_SPLIT_PANE_VERTICAL, 0.5f);
+	dir_listing = UITableCreate(&split1->e, 0, "File Name\tFile Permissions"); 
 	UIPanel *panel = UIPanelCreate(&split1->e, UI_PANEL_GRAY |
 									UI_PANEL_MEDIUM_SPACING);
-	
 	textbox = UITextboxCreate(&panel->e, 0);
-	label = UILabelCreate(&panel->e, 0, "text", -1);
-	dir_listing = UICodeCreate(&split1->e, 0); 
-//	UITable *test_table = UITableCreate(&panel->e, 0, "test\t1\t2\t3\tdone");
-//	test_table->itemCount = 10;
-//	UITableResizeColumns(test_table);
-//	test_table->e.messageUser = MyTableMessage;
-//
-
 	button = UIButtonCreate(&panel->e, 0, "Push", -1);
+	label = UILabelCreate(&panel->e, 0, "text", -1);
 	
 
-	
-	//split = UISplitPaneCreate(&window->e, UI_SPLIT_PANE_VERTICAL, .08f);
-/*
-	{
-		UICode *code = UICodeCreate(&split->e, 0);
-		char *buffer = (char *) malloc(262144);
-		FILE *f = fopen("luigi.h", "rb");
-		size_t size = fread(buffer, 1, 262144, f);
-		fclose(f);
-		UICodeInsertContent(code, buffer, size, true);
-		UICodeFocusLine(code, 0);
-	}
-*/
 	button->e.messageUser = ButtonMessage;
 	textbox->e.messageUser = TextboxMessage;
+	//dir_listing->e.messageUser = TableMessage;
+	dir_listing->e.messageUser = TableContents;
+	dir_listing->itemCount = 2; //debug only for now -- TODO
+	UITableResizeColumns(dir_listing);
+	
 
 	return UIMessageLoop();
 }
@@ -86,57 +81,57 @@ void cleanup_SLL(struct String_Linked_List *l)
 	return;
 }
 
-//caller must free returned result
-//returns number of bytes put into the **result
-int list_dir(const char *path, char **result)
+//replaces the duplicated functionality in list_dir
+//TODO - actually finish refactoring that: remove the duplicated code
+//TODO - transition to using linked list + table in gui rather than string + code block
+int create_dir_SLL(const char *path, struct String_Linked_List *result)
 {
 	struct dirent *entry;
 	DIR *dp;
 	dp = opendir(path);
 	if (dp == NULL) {
-		perror("list-dir: Unable to open directory.");
+		perror("create_dir_sll: Unable to open directory.");
 		return -1;
 	}
-	#define MAX_FNAME_LENGTH 256
+	#define MAX_FNAME_LENGTH 256 
 	char buf[MAX_FNAME_LENGTH];
-	struct String_Linked_List *names = NULL;
-	struct String_Linked_List *end = NULL;
+	struct String_Linked_List *list_head = NULL;
+	struct String_Linked_List *list_current_pos = NULL;
 	while ((entry = readdir(dp))) {
-		//save each entry string
-		//get all their cumulative lengths
-		//combine into one malloc'd block
-		//free up the temporary holdings for those strings
-		
-		//set up the head of the linked list
-		if (names == NULL) {
-			names = (struct String_Linked_List *) malloc(sizeof(struct String_Linked_List)); //MALLOC --
-			names->length = strlen(entry->d_name);
-			names->str = (char *) malloc(sizeof(char) * names->length + 1); //MALLOC --
-			strcpy(names->str, entry->d_name);
-			names->next = NULL;
-			end = names;
+		if (list_head == NULL) {
+			list_head = (struct String_Linked_List *) malloc(sizeof(struct String_Linked_List));
+			list_head->length = strlen(entry->d_name);
+			list_head->str = (char *) malloc(sizeof(char) * list_head->length + 1);
+			strcpy(list_head->str, entry->d_name);
+			list_head->next = NULL;
+			list_current_pos = list_head;
 		} else {
 			struct String_Linked_List *new = malloc(sizeof(struct String_Linked_List)); //MALLOC --
-			end->next = new;
+			list_current_pos->next = new;
 			new->length = strlen(entry->d_name);
 			new->str = (char *) malloc(sizeof(char) * new->length + 1); //MALLOC --
 			strcpy(new->str, entry->d_name);
 			new->next = NULL;
 
-			end = new; //defer'd / must be at end of else block
+			list_current_pos = new; //defer'd / must be at end of else block
 		}
 	}
-	
-	size_t string_bytes = pack_SLL_to_CString(names, result);
-	if (string_bytes < 0) {
-		printf("Panic -- pack_SLL_to_CString failed.\n");
-		exit(-1);
-	}
-	cleanup_SLL(names);
 
-	return string_bytes;
+	result = list_head; //"return" the result
+	return 0;
+} 
+
+//caller must free returned result
+//returns 0 on success
+//kind of useless for now, but later (TODO) will have it maybe modify the SLL to have things
+//like file size, etc in this step rather than in create_dir_SLL, so keeping them separate for now
+int list_dir(const char *path, struct String_Linked_List *result)
+{
+	create_dir_SLL(path, result);
+	return 0;
 }
 
+//NOTE: kind of defunct for now. Deprecated.
 //helper to list_dir()
 //caller is responsible for dynamically allocated *result
 //negative return indicates error, otherwise return is size of string (bytes)
@@ -181,25 +176,68 @@ int pack_SLL_to_CString(struct String_Linked_List *head, char **result)
 
 	return string_total_length;
 }
+/* //note: commented out, using TableContents below for now
+int SELECTED_ELEMENT = 1;
+int TableMessage(UIElement *element, UIMessage message, int di, void *dp)
+{
+	if (message == UI_MSG_TABLE_GET_ITEM) {
+		UITableGetItem *item_ptr = (UITableGetItem *) dp;
+		item_ptr->isSelected = (item_ptr->index == SELECTED_ELEMENT);
+		size_t bytes = snprintf(item_ptr->buffer, item_ptr->bufferBytes, "debug:"); 
+		printf("DEBUG - TableMessage\n");
+		printf("%d bytes into buffer. Contents: %s\n", bytes, item_ptr->buffer);
+		return bytes;
+	}
+}
+*/
 
 //#include <stdio.h> //remove once snprintf is no longer needed
 int ButtonMessage(UIElement *element, UIMessage message, int di, void *dp)
 {
 	if (message == UI_MSG_CLICKED) {
-		//TODO finish implementing properly
-		char *result;
-		size_t listing_bytes = list_dir(".", &result);
-		printf("DEBUG: inserting %d bytes into UICode: dir_listing\n", listing_bytes);
-		UICodeInsertContent(dir_listing, result, listing_bytes, 1);
-		UICodeFocusLine(dir_listing, 0);
+		struct String_Linked_List result;
+		uint8_t list_status = list_dir(".", &result);
+		printf("Made it past list_dir\n");
+		fflush(stdout);
+		memcpy(&Global_Dir_Listing, &result, sizeof(struct String_Linked_List));
+		printf("Made it past memcpy op\n");
+		fflush(stdout);
+		//if (list_status != 0) { printf("list_dir returned nonzero in ButtonMessage\n"); } //TODO -- better handle this, once the error code is actually relevant
+
 		UIElementRefresh(&dir_listing->e);
 		UIElementRefresh(dir_listing->e.parent);
-
 		/* --was used for debugging -- now commented out
 		UILabelSetContent(label, result, listing_bytes);
 		UIElementRefresh(&label->e);
 		UIElementRefresh(label->e.parent);
 		*/
+	}
+	return 0;
+}
+
+int table_selected_element = 1;
+int TableContents(UIElement *element, UIMessage message, int di, void *dp)
+{
+	if (message == UI_MSG_TABLE_GET_ITEM) {
+		UITableGetItem *ptr = (UITableGetItem *) dp;
+		ptr->isSelected = (table_selected_element == ptr->index);
+		struct String_Linked_List *dir_entry = &Global_Dir_Listing;
+		for(int i = 0; i < ptr->index; ++i) {
+			dir_entry = dir_entry->next;
+			if (dir_entry == NULL) break;
+		}
+		size_t bytes = snprintf(ptr->buffer, ptr->bufferBytes,
+			(dir_entry == NULL) ? "NULL" : dir_entry->str);
+		printf("%d bytes into buffer. Contents: %s\n", bytes, ptr->buffer);
+		return bytes;
+	} else if (message == UI_MSG_LEFT_DOWN) {
+		int hit = UITableHitTest((UITable *) element, element->window->cursorX, element->window->cursorY);
+		if (table_selected_element != hit) {
+			table_selected_element = hit;
+			if (!UITableEnsureVisible((UITable *) element, table_selected_element)) {
+				UIElementRepaint(element, NULL);
+			}
+		}
 	}
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,11 @@
 default: main.c
+	gcc -o mexplorer.exe main.c -luser32 -lgdi32 -lshell32
+
+release:
 	gcc -O2 -o mexplorer.exe main.c -luser32 -lgdi32 -lshell32
+
+debug:
+	gcc -g -o mexplorer.exe main.c -luser32 -lgdi32 -lshell32
 
 clean:
 	rm mexplorer.exe
